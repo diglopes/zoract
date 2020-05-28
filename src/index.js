89735c67623a1a57ed454d60eda1015d854c1b47
Diego Lopes <diego-chez@hotmail.com>
2020-05-28 09:51:48 -0300

    refac: move functions out of index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,4 @@
-export const createElement = (type, props, ...children) => {
-  return {
-    type,
-    props: {
-      ...props,
-      children: children.map((child) =>
-        typeof child !== "object" ? createTextElement(child) : child
-      ),
-    },
-  };
-};
+import { render } from "./dom";
+import { createElement } from "./element";
 
-const createTextElement = (text) => {
-  return {
-    type: "TEXT_ELEMENT",
-    props: {
-      nodeValue: text,
-      children: [],
-    },
-  };
-};
-
-export const render = (element, container) => {
-  const dom =
-    element.type === "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(element.type);
-  const isProperty = (key) => key !== "children";
-  Object.keys(element.props)
-    .filter(isProperty)
-    .forEach((name) => {
-      dom[name] = element.props[name];
-    });
-  element.props.children.forEach((child) => render(child, dom));
-  container.appendChild(dom);
-};
+export { render, createElement };
